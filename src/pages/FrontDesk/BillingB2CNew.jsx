2e8a5043148d3f107b28f2a2c509180adeb895c3
rahulxdev0 <rk854327@gmail.com>
2025-05-21 23:30:18 +0530

    add BillingB2CNew component for creating new B2C bills and remove unused index.js file from store

---
+++ src/pages/FrontDesk/BillingB2CNew.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BillingB2CNew() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">New B2C Billing</h1>
+      <p>Create a new B2C bill here.</p>
+    </div>
+  );
+}
+
+export default BillingB2CNew;
